91e1e9c76cc21945de9d1378aeb7b16b944f97b1
Lucas <lucasdsveras@gmail.com>
2021-03-17 11:06:54 +0000

    Initial commit

---
+++ R/example_data.R
@@ -0,0 +1,24 @@
+#' Get path to example data
+#'
+#' \code{impactr} comes with some example ActiGraph accelerometer raw data
+#' files in its `inst/extdata` directory. This function make them easy to
+#' access.
+#'
+#' @param file A character string with the file name. If \code{NULL}, the
+#'   example files will be listed.
+#'
+#' @return If \code{file = NULL}, it returns the file names of the example
+#'   data files, else it returns the path to the example data.
+#'
+#' @export
+#'
+#' @examples
+#' example_data()
+#' example_data("waist-imu.csv")
+example_data <- function(file = NULL) {
+  if (is.null(file)) {
+    dir(system.file("extdata", package = "impactr"))
+  } else {
+    system.file("extdata", file, package = "impactr", mustWork = TRUE)
+  }
+}
