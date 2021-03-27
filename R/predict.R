06216220c94c7073d2063120d32a553d37d200d6
Lucas <lucasdsveras@gmail.com>
2021-03-27 21:20:36 +0000

    Add coefficients for other placements and vectors

--- R/predict.R
+++ R/predict.R
@@ -5,10 +5,20 @@ predict_grf <- function(data, vector) {
   compute_grf(coeff, peaks, body_mass)
 }
 
-get_coefficients <- function(outcome, placement, vector) {
+get_coefficients <- function(outcome, acc_placement, vector) {
   if (outcome == "grf") {
-    if (placement == "hip" & vector == "resultant") {
+    if (acc_placement == "ankle" & vector == "resultant") {
+      list(b0 = 1026.046, b1 = - 153.073, b2 = 6.641, b3 = 2.097)
+    } else if (acc_placement == "hip" & vector == "resultant") {
       list(b0 = 844.500, b1 = - 264.692, b2 = 4.677, b3 = 5.118)
+    } else if (acc_placement == "back" & vector == "resultant") {
+      list(b0 = 795.173, b1 = - 258.882, b2 = 5.951, b3 = 4.903)
+    } else if (acc_placement == "ankle" & vector == "vertical") {
+      list(b0 = 1014.045, b1 = - 226.690, b2 = 4.854, b3 = 3.562)
+    } else if (acc_placement == "back" & vector == "vertical") {
+      list(b0 = 890.428, b1 = - 339.790, b2 = 4.533, b3 = 6.131)
+    } else if (acc_placement == "hip" & vector == "vertical") {
+      list(b0 = 908.037, b1 = - 322.843, b2 = 4.546, b3 = 5.691)
     }
   }
 }
