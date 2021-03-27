180a746836b56509081088d06c6878101f7c2694
Lucas <lucasdsveras@gmail.com>
2021-03-27 21:24:02 +0000

    Rename function to retrieve the coefficients

--- R/predict.R
+++ R/predict.R
@@ -1,24 +1,22 @@
 predict_grf <- function(data, vector) {
-  coeff <- get_coefficients("grf", attributes(data)$acc_placement, vector)
+  coeff <- get_grf_coefficients("grf", attributes(data)$acc_placement, vector)
   peaks <- attributes(data)$peaks[[vector]]$height
   body_mass <- attributes(data)$subj_body_mass
   compute_grf(coeff, peaks, body_mass)
 }
 
-get_coefficients <- function(outcome, acc_placement, vector) {
-  if (outcome == "grf") {
-    if (acc_placement == "ankle" & vector == "resultant") {
-      list(b0 = 1026.046, b1 = - 153.073, b2 = 6.641, b3 = 2.097)
-    } else if (acc_placement == "hip" & vector == "resultant") {
-      list(b0 = 844.500, b1 = - 264.692, b2 = 4.677, b3 = 5.118)
-    } else if (acc_placement == "back" & vector == "resultant") {
-      list(b0 = 795.173, b1 = - 258.882, b2 = 5.951, b3 = 4.903)
-    } else if (acc_placement == "ankle" & vector == "vertical") {
-      list(b0 = 1014.045, b1 = - 226.690, b2 = 4.854, b3 = 3.562)
-    } else if (acc_placement == "back" & vector == "vertical") {
-      list(b0 = 890.428, b1 = - 339.790, b2 = 4.533, b3 = 6.131)
-    } else if (acc_placement == "hip" & vector == "vertical") {
-      list(b0 = 908.037, b1 = - 322.843, b2 = 4.546, b3 = 5.691)
-    }
+get_grf_coefficients <- function(outcome, acc_placement, vector) {
+  if (acc_placement == "ankle" & vector == "resultant") {
+    list(b0 = 1026.046, b1 = - 153.073, b2 = 6.641, b3 = 2.097)
+  } else if (acc_placement == "hip" & vector == "resultant") {
+    list(b0 = 844.500, b1 = - 264.692, b2 = 4.677, b3 = 5.118)
+  } else if (acc_placement == "back" & vector == "resultant") {
+    list(b0 = 795.173, b1 = - 258.882, b2 = 5.951, b3 = 4.903)
+  } else if (acc_placement == "ankle" & vector == "vertical") {
+    list(b0 = 1014.045, b1 = - 226.690, b2 = 4.854, b3 = 3.562)
+  } else if (acc_placement == "back" & vector == "vertical") {
+    list(b0 = 890.428, b1 = - 339.790, b2 = 4.533, b3 = 6.131)
+  } else if (acc_placement == "hip" & vector == "vertical") {
+    list(b0 = 908.037, b1 = - 322.843, b2 = 4.546, b3 = 5.691)
   }
 }
