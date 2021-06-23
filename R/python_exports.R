f9a3425b7cfe1b984063e42767ac9f02651e56d2
Lucas <lucasdsveras@gmail.com>
2021-06-23 15:32:00 +0100

    Add install_scipy()

--- R/python_exports.R
+++ R/python_exports.R
@@ -7,3 +7,19 @@ scipy <- NULL
   # Initialize scipy module .onLoad
   scipy <<- reticulate::import("scipy", delay_load = TRUE)
 }
+
+#' Install SciPy
+#'
+#' Install the SciPy Python package into a virtual environment.
+#'
+#' @param method Installation method. By default, "auto" automatically finds a
+#' method that will work in the local environment.
+#' @param conda The path to a conda executable. Use "auto" to allow reticulate
+#' to automatically find an appropriate conda binary.
+#'
+#' @seealso \code{\link[reticulate:py_install]{py_install()}}
+#'
+#' @export
+install_scipy <- function(method = "auto", conda = "auto") {
+  reticulate::py_install("scipy", method = method, conda = conda)
+}
