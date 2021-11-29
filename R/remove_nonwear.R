e143a446086af835f0a5512f29eb517eb868b61c
Lucas <lucasdsveras@gmail.com>
2021-11-29 16:57:56 +0000

    Start writing step 2 of the algorithm

--- R/remove_nonwear.R
+++ R/remove_nonwear.R
@@ -6,17 +6,15 @@ remove_nonwear <- function(data, window1 = 60, window2 = 15, threshold = 2) {
   n_blocks <- floor(nrow(data) / window2)
   crit <- ((window1 / window2) / 2) + 1
 
-  # Non-wear criteria based on acceleration siganl range and standard deviation
   range_crit <- 0.05
   sd_crit <- 0.013
 
-  # Initialize matrix to hold non-wear values
+  # Step 1
   non_wear <- matrix(0, n_blocks, 3)
 
   for (i in 1:n_blocks) {
     print(i)
     if (i <= crit) {
-      # Beginning of data
       start <- 1
       end <- window1
     } else if (i > crit & i < (n_blocks - crit)) {
@@ -25,7 +23,6 @@ remove_nonwear <- function(data, window1 = 60, window2 = 15, threshold = 2) {
     } else if (i >= (n_blocks - crit)) {
       start <- (n_blocks - crit) * window2
       end <- n_blocks * window2
-      # End of data
     }
 
     for (j in 1:3) {
@@ -41,6 +38,28 @@ remove_nonwear <- function(data, window1 = 60, window2 = 15, threshold = 2) {
       } else {
         non_wear[i, j] <- 1
       }
+    }
+  }
+  non_wear <- rowSums(non_wear)
+
+  # Step 2
+  non_wear_idx <- which(non_wear >= threshold)
+  non_wear_final <- matrix(0, length(non_wear), 1)
+  non_wear_final[non_wear_idx] <- 1
+  non_wear_final <- c(0, non_wear_final, 0)
+
+  start_wear <- which(diff(non_wear_final) == 1) + 1
+  start_non_wear <- which(diff(non_wear_final) == - 1) + 1
+
+  if (length(start_wear) > 1) {
+    length_wear <- matrix(0, length(start_wear) - 1, 1)
+    length_non_wear_after <- matrix(0, length(start_wear) - 1, 1)
+    length_non_wear_before <- matrix(0, length(start_wear) - 1, 1)
+
+    for (i in 1:length(start_wear) - 1) {
+      length_wear[i] <- abs(start_wear[i + 1] - start_non_wear[i])
+      length_non_wear_after <- abs(start_non_wear[i + 1] - start_wear[i])
+      length_non_wear_before <- abs(start_non_wear[i] - start_wear[i])
     }
   }
 
