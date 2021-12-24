cfb2b6d814cdb6117ec4137b41fd6261b12c1dd1
Lucas <lucasdsveras@gmail.com>
2021-12-24 17:35:42 +0000

    Update code to add external data `daily_acc`

--- data-raw/daily_acc.R
+++ data-raw/daily_acc.R
@@ -1,3 +1,9 @@
-## code to prepare `daily_acc` dataset goes here
+zip::unzip("data-raw/acc_data.zip")
 
-usethis::use_data(daily_acc, overwrite = TRUE)
+daily_acc <- read_acc("data-raw/acc_data.csv")  |>
+  define_region(
+    start_time = "2017-01-03 00:01:00",
+    end_time = "2017-01-05 23:59:59"
+  )
+
+usethis::use_data(daily_acc, compress = "xz", overwrite = TRUE)
