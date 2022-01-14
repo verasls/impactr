f66cae8ea5ed99d6603c21e6e67bead0310e1c4e
Lucas <lucasdsveras@gmail.com>
2022-01-14 11:52:50 +0000

    Remove external data

--- data-raw/daily_acc.R
+++
@@ -1,9 +0,0 @@
-zip::unzip("data-raw/acc_data.zip")
-
-daily_acc <- read_acc("data-raw/acc_data.csv")  |>
-  define_region(
-    start_time = "2017-01-03 00:01:00",
-    end_time = "2017-01-05 23:59:59"
-  )
-
-usethis::use_data(daily_acc, compress = "xz", overwrite = TRUE)
